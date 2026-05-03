370ef1dad0a0fe88cd3085d66d1a8cf93ebfa82f
Jakub Scholz <www@scholzj.com>
2026-05-03 15:13:36 +0200

    Check if Maven artfiacts in Connect Build now work on Kind (#12700)
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- systemtest/src/test/java/io/strimzi/systemtest/connect/ConnectBuilderST.java
+++ systemtest/src/test/java/io/strimzi/systemtest/connect/ConnectBuilderST.java
@@ -30,7 +30,6 @@ import io.strimzi.operator.common.Util;
 import io.strimzi.systemtest.AbstractST;
 import io.strimzi.systemtest.Environment;
 import io.strimzi.systemtest.TestConstants;
-import io.strimzi.systemtest.annotations.KindNotSupported;
 import io.strimzi.systemtest.annotations.MicroShiftNotSupported;
 import io.strimzi.systemtest.annotations.OpenShiftOnly;
 import io.strimzi.systemtest.annotations.ParallelTest;
@@ -52,6 +51,7 @@ import io.strimzi.systemtest.utils.kafkaUtils.KafkaConnectUtils;
 import io.strimzi.systemtest.utils.kafkaUtils.KafkaTopicUtils;
 import io.strimzi.systemtest.utils.kubeUtils.objects.NetworkPolicyUtils;
 import io.strimzi.systemtest.utils.kubeUtils.objects.PodUtils;
+import io.strimzi.test.k8s.KubeClusterResource;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.BeforeAll;
@@ -75,6 +75,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assumptions.assumeFalse;
 
 @Tag(REGRESSION)
 @Tag(CONNECT_COMPONENTS)
@@ -532,7 +533,6 @@ class ConnectBuilderST extends AbstractST {
 
     @Tag(SANITY)
     @Tag(ACCEPTANCE)
-    @KindNotSupported("using kind we encounter (error building image: deleting file system after stage 0: unlinkat //product_uuid: device or resource busy)")
     @ParallelTest
     @TestDoc(
         description = @Desc("Test building a plugin using Maven coordinates artifacts."),
@@ -549,6 +549,9 @@ class ConnectBuilderST extends AbstractST {
         }
     )
     void testBuildPluginUsingMavenCoordinatesArtifacts() {
+        // This assumption could be removed when Buildah moves to GA
+        assumeFalse(KubeClusterResource.getInstance().isKind() && !Environment.isConnectBuildWithBuildahEnabled());
+
         final TestStorage testStorage = new TestStorage(KubeResourceManager.get().getTestContext());
 
         final String imageName = getImageNameForTestCase();
